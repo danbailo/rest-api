5c9f75c441aaeed28a29c4cd106c0e9f1ebb1c55
Daniel Bailo <danbailoufms@gmail.com>
2020-05-14 20:50:05 -0300

    returning a list of elements

--- app.py
+++ app.py
@@ -4,9 +4,33 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+hotels = [
+	{
+		"hotel_id": "alpha",
+		"name": "Alpha Hotel",
+		"stars": 4.3,
+		"daily": 420.34,
+		"city": "Rio de Janeiro"
+	},
+	{
+		"hotel_id": "bravo",
+		"name": "Bravo Hotel",
+		"stars": 4.4,
+		"daily": 380.90,
+		"city": "Santa Catarina"
+	},
+	{
+		"hotel_id": "charlie",
+		"name": "Charlie Hotel",
+		"stars": 3.9,
+		"daily": 320.20,
+		"city": "Santa Catarina"
+	}				
+]
+
 class Hotels(Resource):
 	def get(self):
-		return {"hotels": "my hotels"}
+		return {"hotels": hotels}
 
 api.add_resource(Hotels, "/hotels")
 
