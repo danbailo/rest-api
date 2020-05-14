009c4e0527ded6496c67847001a75beff6a7a9d7
Daniel Bailo <danbailoufms@gmail.com>
2020-05-14 20:58:03 -0300

    refactoring the project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # First REST-API
 
+## Annotations
 - Using `Postman` to test the resources.
+- The empty file `__init__.py` inside directory `./resoures/` serves to say that this directory now it's a module of the program.
--- app.py
+++ app.py
@@ -1,36 +1,9 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
+from resources.hotel import Hotels
 
 app = Flask(__name__)
 api = Api(app)
-
-hotels = [
-	{
-		"hotel_id": "alpha",
-		"name": "Alpha Hotel",
-		"stars": 4.3,
-		"daily": 420.34,
-		"city": "Rio de Janeiro"
-	},
-	{
-		"hotel_id": "bravo",
-		"name": "Bravo Hotel",
-		"stars": 4.4,
-		"daily": 380.90,
-		"city": "Santa Catarina"
-	},
-	{
-		"hotel_id": "charlie",
-		"name": "Charlie Hotel",
-		"stars": 3.9,
-		"daily": 320.20,
-		"city": "Santa Catarina"
-	}				
-]
-
-class Hotels(Resource):
-	def get(self):
-		return {"hotels": hotels}
 
 api.add_resource(Hotels, "/hotels")
 
