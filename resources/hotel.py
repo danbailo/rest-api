df05e97c23a46f560cb06730953f29c7ea633f01
Daniel Bailo <danbailoufms@gmail.com>
2020-05-14 22:52:40 -0300

    refactoring the project and the code, I has created a new module for to organize the class model of the resources, that is, the object

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hotels = [
 	{
@@ -24,10 +25,12 @@ hotels = [
 	}				
 ]
 
+#provides resources to all hotels, that is, the info of all hotels
 class Hotels(Resource):
 	def get(self):
 		return {"hotels": hotels}
 
+#provides resources for each hotel, that is, the info from determined hotel
 class Hotel(Resource):
 
 	#get data from client
@@ -52,14 +55,15 @@ class Hotel(Resource):
 	
 	def post(self, hotel_id): #creating a new hotel (register in website)
 		data = Hotel.args.parse_args()
-		new_hotel = {"hotel_id": hotel_id, **data}
+		hotel_object = HotelModel(hotel_id, **data)
+		new_hotel = hotel_object.json()
 		hotels.append(new_hotel) 
 		return new_hotel, 200 #sucess status code
 
 	def put(self, hotel_id):
 		data = Hotel.args.parse_args()
-		new_hotel = {"hotel_id": hotel_id, **data}
-
+		hotel_object = HotelModel(hotel_id, **data)
+		new_hotel = hotel_object.json()
 		hotel = Hotel.find_hotel(hotel_id)
 		if hotel:
 			hotel.update(new_hotel) #dict method to updated data
