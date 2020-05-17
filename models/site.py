108269cf12d78e52a33fa8827f03eb445797d5d2
Daniel Bailo <danbailoufms@gmail.com>
2020-05-17 07:50:50 -0300

    new model of sites

---
+++ models/site.py
@@ -0,0 +1,33 @@
+from sql_alchemy import db
+
+class SiteModel(db.Model):
+    __tablename__ = "sites"
+
+    site_id = db.Column(db.String, primary_key=True)
+    url = db.Column(db.String(64))
+	hotels = db.relationship("HotelModel") #list of Hotels obj
+
+    def __init__(self, site_id):
+        self.url = url
+
+    def json(self):
+        return {
+            "site_id": self.site_id,
+            "url": self.url,
+			"hotels": [hotel.json() for hotel in self.hotels] 
+        } #self.hotels in this context means only the hotels that are a HotelModel object
+
+    @classmethod
+    def find_site(cls, url):
+        site = cls.query.filter_by(url=url).first()
+        if site:
+            return site
+        return None
+
+    def save_site(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_site(self):
+        db.session.delete(self)
+        db.session.commit()
