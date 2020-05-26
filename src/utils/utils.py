6d8d9758eed145b7fc73917cdac234457cf56e3f
Daniel Bailo <danbailoufms@gmail.com>
2020-05-26 02:46:40 -0300

    functions has been removed

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,10 +1,8 @@
 import json
 import os.path
 
-def get_credentials(path=os.path.join("..", "config", ""), input_file="credentials.json"):
-	with open(path + input_file) as file:
-		return json.load(file)
+with open(os.path.join("..", "config", "") + "credentials.json") as file:
+	CREDENTIALS = json.load(file)
 
-def get_db_path(path=os.path.join("..", "config", ""), input_file="database.json"):
-	with open(path + input_file) as file:
-		return json.load(file)		
+with open(os.path.join("..", "config", "") + "database.json") as file:
+	DATABASE = json.load(file)
