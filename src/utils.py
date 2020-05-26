77782814abfc880e931f52889a605bc371e7c6fd
Daniel Bailo <danbailoufms@gmail.com>
2020-05-26 00:37:48 -0300

    change the credentials file format

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,5 @@
-import re
+import json
 
 def get_credentials():
-	pattern = re.compile(r"\w*\s=\s(.*)")
-	with open("src/models/CREDENTIALS.txt") as file:
-		credentials = [pattern.match(line).group(1) for line in file]
-	return credentials
+	with open("config/credentials.json") as file:
+		return json.load(file)
